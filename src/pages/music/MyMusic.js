8a3d8207815aead030474e767ef91fe3c3de0d44
Pedro Vinha <aluno20179@ipt.pt>
2020-04-12 22:06:26 +0100

    continuing My music

--- src/pages/music/MyMusic.js
+++ src/pages/music/MyMusic.js
@@ -1,28 +1,24 @@
 import React from 'react';
 export default class MyMusicPage extends React.Component {
 
-  render () {
+  render() {
     return (
-      <div className="w-full h-full flex justify-left">
+      <div className="w-full h-full flex flex-col items-start px-8 py-4">
 
-      <div className="mx-5">
-        <div>
-          <h1 className="text-4xl text-white font-bold tracking-tight">My Music</h1>
-          <hr className="w- border-gray-800 " />
-        </div>
-        <div className="mt-20 items-center">
-          <div className="w-32 absolute">
-            <img className="absolute h-32  rounded-lg" alt="Album cover" src="https://i.pinimg.com/originals/d9/0d/59/d90d59e74df03ae53756476e990fd425.jpg" />
-          </div>
 
-          <div className="ml-3 leading-tight mx- flex ">
-            <h3 className="font-medium text-md ">Artist</h3>
-            <h2 className="font-bold text-xl">Name of Song</h2>
+
+        <h1 className="text-4xl border-gray-800 border-b text-white font-bold tracking-tight">My Music</h1>
+
+        <div className="mt-12 flex items-center flex-wrap">
+
+          <div className="w-1/6 flex ml-3 leading-tight flex-col items-center ">
+            <img className="rounded-lg" alt="Album cover" src="https://i.pinimg.com/originals/d9/0d/59/d90d59e74df03ae53756476e990fd425.jpg" />
+            <h2 className="mt-2 text-gray-100 font-bold text-lg ">Song Name</h2>
+            <h3 className="text-gray-100 text-xs">Some Amazing Artist</h3>
           </div>
 
         </div>
-        
-      </div>
+
       </div>
 
     );
