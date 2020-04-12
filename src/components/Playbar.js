912ea8efafa899d6289ef7bd7f14f8ea45cd9f4a
tobyselway <tobyselway@outlook.com>
2020-04-12 14:35:48 +0100

    Added Playbar

---
+++ src/components/Playbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export default class PlaybarComponent extends React.Component {
+
+    render() {
+        return (
+            <nav className="bg-gray-300 h-24 px-4 flex text-gray-200 items-center">
+
+                <img className="h-20 w-20" src="https://i.pinimg.com/originals/d9/0d/59/d90d59e74df03ae53756476e990fd425.jpg"/>
+                
+            </nav>
+        );
+    }
+
+}
--- src/core/Router.js
+++ src/core/Router.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Navbar from '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
+import Playbar from '../components/Playbar';
 
 import Home from '../pages/home/Home';
 import About from '../pages/about/About';
@@ -10,22 +11,26 @@ export default class RouterComponent extends React.Component {
   render () {
     return (
       <Router>
-            <div className="bg-gray-200 text-gray-900 antialiased font-sans w-screen h-screen flex">
+            <div className="bg-gray-200 text-gray-900 antialiased font-sans w-screen h-screen flex flex-col">
 
-              <Navbar/>
+              <div className="flex-1 flex">
+                <Sidebar/>
 
-              <main className="w-full h-full">
+                <main className="w-full h-full">
 
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/about">
-                        <About />
-                    </Route>
-                </Switch>
+                  <Switch>
+                      <Route exact path="/">
+                          <Home />
+                      </Route>
+                      <Route path="/about">
+                          <About />
+                      </Route>
+                  </Switch>
 
-              </main>
+                </main>
+              </div>
+
+              <Playbar/>
 
             </div>
         </Router>
