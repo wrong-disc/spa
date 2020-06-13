6981e44885429ba7231aa27719895538d3529fe4
Pedro Vinha <aluno20179@ipt.pt>
2020-06-13 18:18:58 +0100

    auth pages input

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -8,17 +8,35 @@ export default class LoginPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirect: false
+            redirect: false,
+            email:"",
+            password:"",
+            error:""
         };
+    }
+
+    handleEmail = (event) => {
+        this.setState ({
+            email: event.target.value
+        });
+    }
+
+    handlePassword = (event) => {
+        this.setState ({
+            password: event.target.value
+        });
     }
 
     login = () => {
         AuthService
-        .login(prompt('email'), prompt('password'))
+        .login(this.state.email, this.state.password)
         .then(() => this.setState({ redirect: true }))
         .catch(err => {
-            this.setState({ redirect: false });
-            alert(err.message);
+            this.setState({ 
+                redirect: false,
+                error: err.message 
+            });
+           
         })
         .finally(this.props.updateMe);
     }
@@ -31,9 +49,14 @@ export default class LoginPage extends React.Component {
             <div className="w-full h-full flex items-center justify-center">
                         
                 <div className="text-center">
-                    <h1 className="text-6xl font-bold tracking-tight border-gray-900 border-4 px-4 pb-1">Wrong Disc</h1>
+                    <h1 className="text-6xl font-bold tracking-tight border-gray-900 border-4 px-4 pb-1" >Wrong Disc</h1>
                     <h2 className="mt-4 text-3xl font-light tracking-tight">Welcome</h2>
-                    <button onClick={this.login} className="mt-10 bg-gray-800 text-gray-200 py-2 px-8 rounded-full font-bold text-2xl shadow focus:outline-none hover:bg-gray-900 active:bg-gray-700">Login</button>
+                    <p className="text-red-600 text-sm font-bold mt-2"> {this.state.error}</p> 
+                    <div className="flex flex-col mt-2 ">
+                        <input type="email" placeholder="Email Address " className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.email} onChange={this.handleEmail}/>
+                        <input type="password" placeholder="Password" className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.password} onChange={this.handlePassword} />
+                    </div>
+                    <button onClick={this.login} className="mt-8 bg-gray-800 text-gray-200 py-2 px-8 rounded-full font-bold text-2xl shadow focus:outline-none hover:bg-gray-900 active:bg-gray-700">Login</button>
                     <div className="mt-8 text-sm text-gray-800">
                         Don't have an account?
                         <NavLink to="/register">
--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -8,19 +8,53 @@ export default class RegisterPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            redirect: false
+            redirect: false,
+            name:"",
+            email:"",
+            password:"",
+            repassword:"",
+            error:""
         };
+    }
+    handleName = (event) => {
+        this.setState ({
+            name: event.target.value
+        });
+    }
+    handleEmail = (event) => {
+        this.setState ({
+            email: event.target.value
+        });
+    }
+
+    handlePassword = (event) => {
+        this.setState ({
+            password: event.target.value
+        });
+    }
+    handleRePassword = (event) => {
+        this.setState ({
+            repassword: event.target.value
+        });
     }
 
     register = () => {
-        AuthService
-        .register(prompt('name'), prompt('email'), prompt('password'))
-        .then(() => this.setState({ redirect: true }))
-        .catch(err => {
-            this.setState({ redirect: false });
-            alert(err.message);
-        })
-        .finally(this.props.updateMe);
+        if(this.state.password != this.state.repassword){
+            this.setState({
+                error: "Passwords do not match"
+            });
+        }else{
+            AuthService
+            .register(this.state.name, this.state.email, this.state.password)
+            .then(() => this.setState({ redirect: true }))
+            .catch(err => {
+                this.setState({ 
+                    redirect: false,
+                    error: err.message 
+                });
+            })
+            .finally(this.props.updateMe);
+        }
     }
 
     render () {
@@ -33,6 +67,13 @@ export default class RegisterPage extends React.Component {
                 <div className="text-center">
                     <h1 className="text-6xl font-bold tracking-tight border-gray-900 border-4 px-4 pb-1">Wrong Disc</h1>
                     <h2 className="mt-4 text-3xl font-light tracking-tight">Create Account</h2>
+                    <p className="text-red-600 text-sm font-bold mt-2"> {this.state.error}</p> 
+                    <div className="flex flex-col mt-2 ">
+                        <input type="text" placeholder="Full Name" className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.name} onChange={this.handleName}/>
+                        <input type="email" placeholder="Email Address " className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.email} onChange={this.handleEmail}/>
+                        <input type="password" placeholder="Password" className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.password} onChange={this.handlePassword} />
+                        <input type="password" placeholder="Repeat Password" className="mt-4 py-2 px-4 rounded-full focus:outline-none shadow focus:bg-gray-300" value={this.state.repassword} onChange={this.handleRePassword} />
+                    </div>
                     <button onClick={this.register} className="mt-10 bg-gray-800 text-gray-200 py-2 px-8 rounded-full font-bold text-2xl shadow focus:outline-none hover:bg-gray-900 active:bg-gray-700">Register</button>
                     <div className="mt-8 text-sm text-gray-800">
                         Already have an account?
